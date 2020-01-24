4ba8dc772cb10fa9a2b4805bce5e1e60c1ec626f
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-01-24 16:38:32 +0200

    убрал дублирование. по-прежнему говнокод

--- AmoebaGameMatcherServer/Services/GameMatcherForceRoomCreator.cs
+++ AmoebaGameMatcherServer/Services/GameMatcherForceRoomCreator.cs
@@ -8,16 +8,17 @@ using AmoebaGameMatcherServer.Experimental;
 //TODO говнокод
 //TODO дублирование кода
 
-
 namespace AmoebaGameMatcherServer.Services
 {
     public class GameMatcherForceRoomCreator
     {
         private readonly GameMatcherDataService dataService;
+        private GameMatcherService gameMatcherService;
         
-        public GameMatcherForceRoomCreator(GameMatcherDataService dataService)
+        public GameMatcherForceRoomCreator(GameMatcherDataService dataService, GameMatcherService gameMatcherService)
         {
             this.dataService = dataService;
+            this.gameMatcherService = gameMatcherService;
         }
 
         public void StartPeriodicCreationInAnotherThread()
@@ -45,7 +46,7 @@ namespace AmoebaGameMatcherServer.Services
                 if (deltaTime.TotalSeconds > Globals.maxStandbyTimeSec)
                 {
                     //есть человек, который долго ждёт
-                    ForceCreateRoom();
+                    gameMatcherService.CreateRoom(Globals.NumbersOfPlayersInRoom);
                 }
                 else
                 {
@@ -58,76 +59,6 @@ namespace AmoebaGameMatcherServer.Services
             }
         }
 
-        /// <summary>
-        /// Достаёт всех игроков из очереди в комнату. Если игроков меньше n, то дополняет ботами.
-        /// </summary>
-        private void ForceCreateRoom()
-        {
-            //Достать n игроков
-            var playersInfo = GetPlayersFromQueue(Globals.NumbersOfPlayersInRoom);
-            if(playersInfo.Count==0)
-                throw new Exception("При принуждённой сборке комнаты не удалось найти свободных игроков");
-
-            int botsCount = Globals.NumbersOfPlayersInRoom - playersInfo.Count;
-            
-            //Дополнить остаток ботами
-            AddBotsToList(ref playersInfo, botsCount);
-            int newRoomNumber = GameRoomIdGenerator.CreateGameRoomNumber();
-            
-            //Создать комнату
-            AddPlayersToTheListOfPlayersInBattle(playersInfo, newRoomNumber);
-            GameRoomData gameRoomData = new GameRoomData
-            {
-                Players = playersInfo.ToArray(),
-                GameRoomNumber = newRoomNumber,
-                GameServerIp = "localhost:48956/"
-            };
-            dataService.GameRoomsData.TryAdd(gameRoomData.GameRoomNumber, gameRoomData);
-        }
-
-        void AddBotsToList(ref List<PlayerInfoForGameRoom> players, int botsCount)
-        {
-            Random random = new Random();
-            for (int i = 0; i < botsCount; i++)
-            {
-                var dich = new PlayerInfoForGameRoom
-                {
-                    IsBot = true,
-                    PlayerLogin = "Рандомное имя для бота "+ random.Next(1,Int32.MaxValue) 
-                };
-                players.Add(dich);
-            }
-        }
-     
-
-        List<PlayerInfoForGameRoom> GetPlayersFromQueue(int numberOfPlayers)
-        {
-            List<PlayerInfoForGameRoom> playersInfo = new List<PlayerInfoForGameRoom>();
-            for (int i = 0; i < numberOfPlayers; i++)
-            {
-                if (dataService.UnsortedPlayers.TryDequeue(out var playerRequest))
-                {
-                    var dich = new PlayerInfoForGameRoom
-                    {
-                        IsBot = false,
-                        PlayerLogin = playerRequest.PlayerId
-                    };
-                    playersInfo.Add(dich);
-                }
-                else
-                {
-                    break;
-                }
-            }
-            return playersInfo;
-        }
-
-        void AddPlayersToTheListOfPlayersInBattle(List<PlayerInfoForGameRoom> players, int gameRoomNumber)
-        {
-            foreach (var player in players)
-            {
-                dataService.PlayersInGameRooms.TryAdd(player.PlayerLogin, gameRoomNumber);
-            }
-        }
+        
     }
 }
--- AmoebaGameMatcherServer/Services/GameMatcherService.cs
+++ AmoebaGameMatcherServer/Services/GameMatcherService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using AmoebaGameMatcherServer.Experimental;
 
 //TODO говнокод
+//TODO слишком большой класс
 
 namespace AmoebaGameMatcherServer.Services
 {
@@ -40,24 +41,71 @@ namespace AmoebaGameMatcherServer.Services
         void TryCreateRoom()
         {
             Console.WriteLine("TryCreateRoom");
-            if (dataService.UnsortedPlayers.Count >= Globals.NumbersOfPlayersInRoom)
+            bool thereIsAFullSetOfPlayers = dataService.UnsortedPlayers.Count >= Globals.NumbersOfPlayersInRoom;
+            if ( thereIsAFullSetOfPlayers) CreateRoom(Globals.NumbersOfPlayersInRoom);
+        }
+
+        /// <summary>
+        /// Создаёт комнату если есть хотя бы один игрок.
+        /// </summary>
+        public void CreateRoom(int maxNumberOfPlayers)
+        {
+            //Достать игроков maxNumberOfPlayers игрокв
+            var playersInfo = GetPlayersFromQueue(maxNumberOfPlayers);
+            if (playersInfo.Count == 0)
             {
-                var playersInfo = GetPlayersFromQueue(Globals.NumbersOfPlayersInRoom);
-                int newRoomNumber = GameRoomIdGenerator.CreateGameRoomNumber();
-
-                AddPlayersToTheListOfPlayersInBattle(playersInfo, newRoomNumber);
+                throw new Exception("Не удалось достать игроков из коллекции. " +
+                                    "При принудительном создании комнаты");
+            }
                 
-                GameRoomData gameRoomData = new GameRoomData
-                {
-                    Players = playersInfo.ToArray(),
-                    GameRoomNumber = newRoomNumber,
-                    GameServerIp = "localhost:48956/"
-                };
+            //Дополнить коллекцию ботами, если не хватает
+            if (playersInfo.Count != maxNumberOfPlayers)
+            {
+                int botsCount = maxNumberOfPlayers - playersInfo.Count; 
+                AddBotsToList(ref playersInfo, botsCount);
+            }
+            
+            //Создать комнату с игроками
+            int newRoomNumber = GameRoomIdGenerator.CreateGameRoomNumber();
+            GameRoomData gameRoomData = new GameRoomData
+            {
+                Players = playersInfo.ToArray(),
+                GameRoomNumber = newRoomNumber,
+                GameServerIp = "localhost:48956/"
+            };
+            
+            //Поставить игрокам статус "В бою"
+            bool success = TryAddPlayersToTheListOfPlayersInBattle(playersInfo, newRoomNumber);
 
-                dataService.GameRoomsData.TryAdd(gameRoomData.GameRoomNumber, gameRoomData);
+            if(!success)
+                throw new Exception("Не удалось поставить всем игрокам статус \"В бою\"ю");
+            
+            //Положить кооманту в коллекцию
+            if (dataService.GameRoomsData.TryAdd(gameRoomData.GameRoomNumber, gameRoomData))
+            {
+                //всё отработало нормально
+            }
+            else
+            {
+                //не удалось положить комнату в коллекцию
+                throw new Exception("Не удалось положить комнату в коллекцию");
             }
         }
 
+        void AddBotsToList(ref List<PlayerInfoForGameRoom> players, int botsCount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < botsCount; i++)
+            {
+                var dich = new PlayerInfoForGameRoom
+                {
+                    IsBot = true,
+                    PlayerLogin = "Рандомное имя для бота "+ random.Next(1,Int32.MaxValue).ToString() 
+                };
+                players.Add(dich);
+            }
+            
+        }
         List<PlayerInfoForGameRoom> GetPlayersFromQueue(int numberOfPlayers)
         {
             List<PlayerInfoForGameRoom> playersInfo = new List<PlayerInfoForGameRoom>();
@@ -77,12 +125,21 @@ namespace AmoebaGameMatcherServer.Services
             return playersInfo;
         }
         
-        void AddPlayersToTheListOfPlayersInBattle(List<PlayerInfoForGameRoom> players, int gameRoomNumber)
+        bool TryAddPlayersToTheListOfPlayersInBattle(List<PlayerInfoForGameRoom> players, int gameRoomNumber)
         {
+            bool success = true;
             foreach (var player in players)
             {
-                dataService.PlayersInGameRooms.TryAdd(player.PlayerLogin, gameRoomNumber);
+                if (dataService.PlayersInGameRooms.TryAdd(player.PlayerLogin, gameRoomNumber))
+                {
+                    
+                }
+                else
+                {
+                    success = false;
+                }
             }
+            return success;
         }
 
         public bool PlayerInQueue(string playerId)
