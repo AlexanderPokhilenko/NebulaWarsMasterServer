12f4d380daedf1d33a4b8564e99cc10d2bde6145
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-02-17 02:25:12 +0200

    поменял начальный id для игрока в комнате на не нулевой

--- AmoebaGameMatcherServer/Services/GameMatcherService.cs
+++ AmoebaGameMatcherServer/Services/GameMatcherService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AmoebaGameMatcherServer.Experimental;
+using AmoebaGameMatcherServer.Utils;
 using Microsoft.CodeAnalysis;
 using NetworkLibrary.NetworkLibrary.Http;
 
--- AmoebaGameMatcherServer/Utils/PlayersTemporaryIdGenerator.cs
+++ AmoebaGameMatcherServer/Utils/PlayersTemporaryIdGenerator.cs
@@ -1,8 +1,8 @@
-﻿namespace AmoebaGameMatcherServer.Experimental
+﻿namespace AmoebaGameMatcherServer.Utils
 {
     public static class PlayersTemporaryIdGenerator
     {
-        private static int lastPlayerId=0;
+        private static int lastPlayerId = 888_777;
         public static int GetPlayerId()
         {
             return lastPlayerId++;
