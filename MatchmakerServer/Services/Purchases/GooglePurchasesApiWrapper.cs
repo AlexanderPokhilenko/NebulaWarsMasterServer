7af2b973ece5df245b9dcab84d05ee5f22d88123
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-03-20 11:52:10 +0200

    minor fixes + авторизация на телефонах перестала работать

--- MatchmakerServer/Services/Purchases/GooglePurchasesApiWrapper.cs
+++ MatchmakerServer/Services/Purchases/GooglePurchasesApiWrapper.cs
@@ -54,7 +54,7 @@ namespace AmoebaGameMatcherServer.Services
         private static string CreateUrlForPurchaseCheck(string sku, string token, string accessToken)
         {
             string result =
-                $"https://www.googleapis.com/androidpublisher/v3/applications/{GoogleApiGlobals.PackageName}/purchases/products/{sku}/tokens/{token}?access_token={accessToken}";
+                $"https://www.googleapis.com/androidpublisher/v3/applications/{GoogleApiGlobals.PackageName}/purchases/products/{sku}/tokens/{token}?access_token="+accessToken;
             return result;
         }
         
