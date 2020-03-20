f7f16ff3aae65be508440edb1e95942b65f333a1
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-03-20 18:20:51 +0200

    ъъ

--- MatchmakerServer/Services/Purchases/GooglePurchasesApiWrapper.cs
+++ MatchmakerServer/Services/Purchases/GooglePurchasesApiWrapper.cs
@@ -54,8 +54,11 @@ namespace AmoebaGameMatcherServer.Services
         
         private static string CreateUrlForPurchaseCheck(string sku, string token, string accessToken)
         {
+            // string result =
+            //     $"https://www.googleapis.com/androidpublisher/v3/applications/{GoogleApiGlobals.PackageName}/purchases/products/{sku}/tokens/{token}?access_token="+accessToken;
+            
             string result =
-                $"https://www.googleapis.com/androidpublisher/v3/applications/{GoogleApiGlobals.PackageName}/purchases/products/{sku}/tokens/{token}?access_token="+accessToken;
+                $"https://www.googleapis.com/androidpublisher/v3/applications/{GoogleApiGlobals.PackageName}/purchases/products/{sku}/tokens/{token}/?access_token={accessToken}";
             return result;
         }
         
