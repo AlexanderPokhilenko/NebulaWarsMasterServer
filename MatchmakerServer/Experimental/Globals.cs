20f4daae4d3efa2ea44453af3c4142c6af242409
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-05-08 22:04:48 +0300

    изменил ip игрового сервера на боевой

--- MatchmakerServer/Experimental/Globals.cs
+++ MatchmakerServer/Experimental/Globals.cs
@@ -6,7 +6,8 @@
         public static readonly int GameVersionNumber = 65;
         public const int NumbersOfPlayersInBattleRoyaleMatch = 10;
         public const int MaxStandbyTimeSec = 10;
-        public const string DefaultGameServerIp = "127.0.0.1";
+        // public const string DefaultGameServerIp = "127.0.0.1";
+        public const string DefaultGameServerIp = "51.144.49.111";
         public const int DefaultGameServerHttpPort = 14065;
         public const int DefaultGameServerUdpPort = 48956;
         public const string SecretKey = "aksjdbskdjfpa8f2p938f23-028mz3029m3fzofpaibef9849242-03i2039d203d2-0d3";
