d83e6dd4b8bce7089d14e2d6b15a6ccd2d3c6361
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-06-12 18:49:12 +0300

    minor fixes

--- MatchmakerServer/Services/GoogleApi/OrderConfirmationService.cs
+++ MatchmakerServer/Services/GoogleApi/OrderConfirmationService.cs
@@ -13,22 +13,30 @@ public class OrderConfirmationService
     {
         this.dbContext = dbContext;
     }
-    
-    public async Task<bool> TryConfirmOrder([NotNull] string serviceId ,[NotNull] string sku)
-    {
-        var purchase = await dbContext.Purchases
-            .Include(purchase1 => purchase1.Account)
-            .Where(purchase1 => purchase1.Account.ServiceId==serviceId&& purchase1.Sku==sku && !purchase1.IsConfirmed)
-            .SingleOrDefaultAsync();
 
-        if (purchase != null)
+    public async Task<bool> TryConfirmOrder([NotNull] string serviceId, [NotNull] string sku)
+    {
+        try
         {
-            purchase.IsConfirmed = true;
-            await dbContext.SaveChangesAsync();
-            Console.WriteLine($"Успешная пометка продукта {purchase.Sku}");
-            return true;
+            var purchase = await dbContext.Purchases
+                .Include(purchase1 => purchase1.Account)
+                .Where(purchase1 =>
+                    purchase1.Account.ServiceId == serviceId && purchase1.Sku == sku && !purchase1.IsConfirmed)
+                .SingleOrDefaultAsync();
+
+            if (purchase != null)
+            {
+                purchase.IsConfirmed = true;
+                await dbContext.SaveChangesAsync();
+                Console.WriteLine($"Успешная пометка продукта {purchase.Sku}");
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message+" "+e.StackTrace);
         }
 
         return false;
-    }
+}
 }
