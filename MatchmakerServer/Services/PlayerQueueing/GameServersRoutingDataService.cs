90f0a7b0fbd63ee880ebd739489f4896f0d580c8
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-12-22 16:02:33 +0200

    Добавлено взаимодействие с монитором игры.

--- MatchmakerServer/Services/PlayerQueueing/GameServersRoutingDataService.cs
+++ MatchmakerServer/Services/PlayerQueueing/GameServersRoutingDataService.cs
@@ -1,4 +1,5 @@
-﻿using AmoebaGameMatcherServer.Experimental;
+﻿using System.Net.Http;
+using AmoebaGameMatcherServer.Experimental;
 
 namespace AmoebaGameMatcherServer.Services.PlayerQueueing
 {
@@ -6,10 +7,21 @@ namespace AmoebaGameMatcherServer.Services.PlayerQueueing
     {
         public MatchRoutingData GetGameServerAddress()
         {
-            MatchRoutingData result = 
-                new MatchRoutingData(Globals.DefaultGameServerIp, Globals.DefaultGameServerUdpPort);
-                
-            return result;
+            using (var httpClient = new HttpClient())
+            {
+                const string url = Globals.GetServerAddressUrl + "?secret=" + Globals.GameServerSecret;
+                var response = httpClient.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var address = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(address))
+                    {
+                        return new MatchRoutingData(address, Globals.DefaultGameServerUdpPort);
+                    }
+                }
+            }
+
+            return new MatchRoutingData(Globals.DefaultGameServerIp, Globals.DefaultGameServerUdpPort);
         }
     }
 }
